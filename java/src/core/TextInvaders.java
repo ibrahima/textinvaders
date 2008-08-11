a1afd029248e4768b53f8d6551cbfe3ef2cf2798
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2008-08-11 08:55:06 +0000

    Pausing works now. Fonts should be stored in the GUI class, currently they're in Collidable which makes no sense but I will fix that later.

--- java/src/core/TextInvaders.java
+++ java/src/core/TextInvaders.java
@@ -11,9 +11,10 @@ public class TextInvaders extends Thread{
 	public KeyBoardState kb;
 	ArrayList<Enemy> enemies;
 	static protected int money, score;
-	protected boolean paused=false;
+	protected GameState state=GameState.GAME;
+	
     public TextInvaders(){
-    	gui = new GUI();
+    	gui = new GUI(this);
     	kb=new KeyBoardState(gui);
     	ship = new Ship();
     	gui.ship=this.ship;
@@ -25,54 +26,80 @@ public class TextInvaders extends Thread{
     	this.start();
     }
     public void run(){
-    	//menu code
     	while(true){
-    		//check keyboard and then move ship accordingly
-    		if(kb!=null) {
-    			HashMap<String, Boolean> keys=kb.keysDown();
-    			Iterator<String> kiter=keys.keySet().iterator();			
-    			while(kiter.hasNext()){
-    				String k=kiter.next();
-    				if(k!=null&&k.equals("Up")) ship.up();
-    				else if(k!=null&&k.equals("Down")) ship.down();
-    				else if(k!=null&&k.equals("Left")) ship.left();
-    				else if(k!=null&&k.equals("Right")) ship.right();
-    				else if(k!=null&&k.equals("Space")) ship.shoot();
-    				else if(k!=null&&k.equals("Escape")){
-    					if(!keys.get(k)){
-	    					paused=!paused;
-	    					keys.put(k, true);
-	    					gui.addMsg("Paused = "+paused);
-    					}
-    				}
-    			}
+    		switch(state){
+				case MAINMENU:
+		        	//menu code
+					break;
+				case GAME:
+		    		//check keyboard and then move ship accordingly
+		    		gui.update();
+		    		if(kb!=null) {
+		    			HashMap<String, Boolean> keys=kb.keysDown();
+		    			Iterator<String> kiter=keys.keySet().iterator();			
+		    			while(kiter.hasNext()){
+		    				String k=kiter.next();
+		    				if(k!=null&&k.equals("Up")) ship.up();
+		    				else if(k!=null&&k.equals("Down")) ship.down();
+		    				else if(k!=null&&k.equals("Left")) ship.left();
+		    				else if(k!=null&&k.equals("Right")) ship.right();
+		    				else if(k!=null&&k.equals("Space")) ship.shoot();
+		    				else if(k!=null&&k.equals("Escape")){
+		    					if(!keys.get(k)){
+			    					state=GameState.PAUSED;
+			    					keys.put(k, true);
+		    					}
+		    				}
+		    			}
+		    		}
+		    		if(ship!=null){
+		    			ship.move();
+		    		}
+		    		//check for collisions
+		        	Iterator<Enemy> enemIter=enemies.iterator();
+		        	while(enemIter.hasNext()){
+		        		//check for collisions with ship, then ship's bullets, then enemy's bullets with ship
+		        		Enemy e=enemIter.next();
+		        		e.move();
+		        		if(ship.checkCollision(e)){
+		        			e.collide(ship);
+		        			ship.collide(e);
+		        		}
+		        		//ship's bullets
+		        		Iterator<Bullet> bs=ship.getBullets();
+		        		while(bs.hasNext()){
+		            		Bullet b=bs.next();
+		            		if(e.checkCollision(b)){
+		            			e.collide(b);
+		            			b.collide(e);
+		            		}        			
+		        		}
+		        		//TODO: enemy's bullets with ship
+		        		if(e.health<=0)enemIter.remove();
+		        	}
+		        	score+=ship.tempscore;
+		        	money+=ship.tempscore;
+		        	ship.tempscore=0;
+					break;
+				case PAUSED:
+		    		if(kb!=null) {
+		    			HashMap<String, Boolean> keys=kb.keysDown();
+		    			Iterator<String> kiter=keys.keySet().iterator();			
+		    			while(kiter.hasNext()){
+		    				String k=kiter.next();
+		    				if(k!=null&&k.equals("Escape")){
+		    					if(!keys.get(k)){
+			    					state=GameState.GAME;
+			    					keys.put(k, true);
+		    					}
+		    				}
+		    			}
+		    		}
+		    		gui.update();
+					break;
+				default:
+					gui.addMsg("Oops, unknown game state reached. Bug the developer.");
     		}
-    		gui.update();
-    		//check for collisions
-        	Iterator<Enemy> enemIter=enemies.iterator();
-        	while(enemIter.hasNext()){
-        		//check for collisions with ship, then ship's bullets, then enemy's bullets with ship
-        		Enemy e=enemIter.next();
-        		if(ship.checkCollision(e)){
-        			e.collide(ship);
-        			ship.collide(e);
-        		}
-        		//ship's bullets
-        		Iterator<Bullet> bs=ship.getBullets();
-        		while(bs.hasNext()){
-            		Bullet b=bs.next();
-            		if(e.checkCollision(b)){
-            			e.collide(b);
-            			b.collide(e);
-            		}        			
-        		}
-        		//TODO: enemy's bullets with ship
-        		if(e.health<=0)enemIter.remove();
-        	}
-        	score+=ship.tempscore;
-        	money+=ship.tempscore;
-        	ship.tempscore=0;
     	}
-    	
     }
 }
